3afb430309fe236796f956587cfdf2f8d4428b84
TaahaaGul <tahagulll06@gmail.com>
2023-04-10 16:04:30 +0300

    UserResponse and Global Exception Handler added

---
+++ src/main/java/com/taahaagul/demo/responses/UserResponse.java
@@ -0,0 +1,16 @@
+package com.taahaagul.demo.responses;
+
+import com.taahaagul.demo.entities.User;
+import lombok.Data;
+
+@Data
+public class UserResponse {
+
+    Long id;
+    String userName;
+
+    public UserResponse(User entity) {
+        this.id = entity.getId();
+        this.userName = entity.getUsername();
+    }
+}
